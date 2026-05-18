61ed593e2dc7640b58ac823365a1a2d0d675cbde
Messias Moisley <moisleyalexandre@gmail.com>
2026-05-18 10:55:01 -0400

    subindo colaborador pro online

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ===============================
 // CONEXÃO MYSQL
